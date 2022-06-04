bbef8e021fe5887e8ac86a52db1bb92ec81907a9
Boyd Johnson <johnson.boyd@gmail.com>
2022-06-04 21:54:28 +0000

    Add maxf maxf_keep_dims

--- src/ops.rs
+++ src/ops.rs
@@ -1,7 +1,9 @@
 //! Ops are mathematical operations on ndarray Arrays.
 
 mod exp;
+mod max;
 mod sigmoid;
 
 pub use exp::{exp, Exp};
+pub use max::{maxf, maxf_keep_dims, Maxf, MaxfKeepDims};
 pub use sigmoid::{sigmoid, Sigmoid};
