ed21bdaa62358c28495f0a445386fe2f94e2a5c6
Boyd Johnson <johnson.boyd@gmail.com>
2022-06-05 20:30:40 +0000

    Add argsortf function and trait to ops

--- src/ops.rs
+++ src/ops.rs
@@ -8,6 +8,7 @@ pub mod sigmoid;
 pub mod softmax;
 
 pub use argmax::{argmaxf, argmaxf_keep_dims, Argmaxf, ArgmaxfKeepDims};
+pub use argsort::{argsortf, Argsortf};
 pub use exp::{exp, Exp};
 pub use max::{maxf, maxf_keep_dims, Maxf, MaxfKeepDims};
 pub use sigmoid::{sigmoid, Sigmoid};
